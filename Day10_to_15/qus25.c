6ff61b9114c6553b9cec407742acb3b1e0fb97ad
DeveshJoshi <deveshjoshidev@gmail.com>
2025-09-02 07:06:59 +0530

    Add files via upload

---
+++ Day10_to_15/qus25.c
@@ -0,0 +1,63 @@
+// Q25: Write a program to implement a basic calculator using switch-case for +, -, *, /, %.
+
+#include<stdio.h>
+
+int main(){
+    char operation;
+    int fir, sec;
+    printf("Enter first number: \n");
+    scanf("%d", &fir);
+    printf("Enter second number: \n");
+    scanf("%d", &sec);
+
+    printf("Enter the operation you want to perform.\n");
+    scanf(" %c", &operation);
+
+    switch(operation){
+        case '+':
+            printf("Sum of %d and %d = %d", fir, sec, fir+sec);
+            break;
+        case '-':
+            printf("Difference of %d and %d = %d", fir, sec, fir-sec);
+            break;
+        case '*':
+            printf("Product of %d and %d = %d", fir, sec, fir*sec);
+            break;
+        case '/':
+            if (sec !=0)
+                printf("Quotnent of %d and %d when dividded is = %d", fir, sec, fir/sec);
+            else
+                printf("Denominator should not be zero!\n");
+            break;
+        case '%':
+            if (sec !=0)
+                printf("Remainder of %d and %d when divided = %d", fir, sec, fir%sec);
+            else
+                printf("Denominator should not be zero!\n");
+            break;
+        default:
+            printf("Invalid Input!");
+        }
+    return 0;
+}
+
+
+
+/*
+Sample Test Cases:
+Input 1:
+4 2 +
+Output 1:
+6
+
+Input 2:
+10 3 %
+Output 2:
+1
+
+Input 3:
+15 5 /
+Output 3:
+3
+
+*/
